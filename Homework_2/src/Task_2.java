cc08974d363cf452ce288b3b1469124cf2d5f883
APanRU <eklmn2004@mail.ru>
2022-12-18 15:18:01 +0300

    add_Homework_2_Task2

--- Homework_2/src/Task_2.java
+++ Homework_2/src/Task_2.java
@@ -1,2 +1,27 @@
+/**
+ * 2. Напишите метод, который определит тип (расширение) файлов из текущей папки
+ * и выведет в консоль результат вида
+ * 1 Расширение файла: txt
+ * 2 Расширение файла: pdf
+ * 3 Расширение файла: jpg
+ * "\"C:\\Users\\Панкратовы\\Desktop\\1\""
+ */
+
+import java.io.File;
+
 public class Task_2 {
+    public static void main(String[] args) {
+        File dir = new File("C:\\Users\\Панкратовы\\Desktop\\1");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+//            System.out.println(file);
+            String name = file.getName();
+//            System.out.println(name);
+            int index = name.lastIndexOf(".");
+//            System.out.println(index);
+            String extension = name.substring(index + 1, name.length());
+            System.out.printf("Расширение файла %s => %s\n", name, extension);
+        }
+    }
+
 }
